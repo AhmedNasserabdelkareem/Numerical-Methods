9cad2864e68fdc25cccffe5e3504a73f44cf6bcd
shazly333 <shazly0120100@gmail.com>
2018-05-10 11:23:52 +0200

    Connect False Position to GUI

--- falsePosition.m
+++ falsePosition.m
@@ -1,12 +1,8 @@
-function [ iter,l,u,r,ea] = falsePosition(func,xl,xu,es,imax)
+function [l,u,r,ea, f] = falsePosition(func,xl,xu,es,imax)
 %using known false position method to find a root inside some interval
 %   it is used to find odd number of roots inside some interval
     f = str2func(['@(x)' vectorize(char(func))]);
-    iter = (1:imax)';
-    l = inf(imax,1);
-    u = inf(imax,1);
-    r = inf(imax,1);
-    ea = inf(imax,1);
+
       
     if(f(xu)*f(xl))> 0.0
         disp('false-point method : no roots in iterval');
