000da93d1481c482bfb895343d813a723047d080
ahmednasser <ahmednasser2337@gmail.com>
2018-05-10 17:22:21 +0200

    add interpolation file

--- interpolation.m
+++ interpolation.m
@@ -1,35 +1,35 @@
-function varargout = untitled(varargin)
-% UNTITLED MATLAB code for untitled.fig
-%      UNTITLED, by itself, creates a new UNTITLED or raises the existing
+function varargout = interpolation(varargin)
+% INTERPOLATION MATLAB code for interpolation.fig
+%      INTERPOLATION, by itself, creates a new INTERPOLATION or raises the existing
 %      singleton*.
 %
-%      H = UNTITLED returns the handle to a new UNTITLED or the handle to
+%      H = INTERPOLATION returns the handle to a new INTERPOLATION or the handle to
 %      the existing singleton*.
 %
-%      UNTITLED('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in UNTITLED.M with the given input arguments.
+%      INTERPOLATION('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in INTERPOLATION.M with the given input arguments.
 %
-%      UNTITLED('Property','Value',...) creates a new UNTITLED or raises the
+%      INTERPOLATION('Property','Value',...) creates a new INTERPOLATION or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before untitled_OpeningFcn gets called.  An
+%      applied to the GUI before interpolation_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to untitled_OpeningFcn via varargin.
+%      stop.  All inputs are passed to interpolation_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help untitled
+% Edit the above text to modify the response to help interpolation
 
-% Last Modified by GUIDE v2.5 07-May-2018 16:33:17
+% Last Modified by GUIDE v2.5 10-May-2018 16:38:16
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @untitled_OpeningFcn, ...
-                   'gui_OutputFcn',  @untitled_OutputFcn, ...
+                   'gui_OpeningFcn', @interpolation_OpeningFcn, ...
+                   'gui_OutputFcn',  @interpolation_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before untitled is made visible.
-function untitled_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before interpolation is made visible.
+function interpolation_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to untitled (see VARARGIN)
+% varargin   command line arguments to interpolation (see VARARGIN)
 
-% Choose default command line output for untitled
+% Choose default command line output for interpolation
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes untitled wait for user response (see UIRESUME)
+% UIWAIT makes interpolation wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = untitled_OutputFcn(hObject, eventdata, handles) 
+function varargout = interpolation_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -71,77 +71,6 @@ function varargout = untitled_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
-
-
-% --- Executes on selection change in listboxpart2.
-function listboxpart2_Callback(hObject, eventdata, handles)
-% hObject    handle to listboxpart2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% index = get(hObject,'Value');
-% list = get(hObject,'String');
-% method = list{index}; 
-index = get(hObject,'Value');
-list = get(hObject,'String');
-method = list{index}; 
-set(handles.text6,'String',method);
-% Hints: contents = cellstr(get(hObject,'String')) returns listboxpart2 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from listboxpart2
-
-
-% --- Executes during object creation, after setting all properties.
-function listboxpart2_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to listboxpart2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: listbox controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-
-% --- Executes on button press in pushbutton1.
-function pushbutton1_Callback(hObject, eventdata, handles)
-% [file,path] = uigetfile('*.m');
-% if isequal(file,0)
-%    disp('User selected Cancel');
-% else
-%    disp(['User selected ', fullfile(path,file)]);
-% end
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --- Executes on button press in pushbutton2.
-function pushbutton2_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton2 (see GCBO)
-%NASSER HERE PARSING POINTS AND CORRESPONDING AND ORDER POINTS 
-global chosenMethod;
-global order;
-global points;
-global correspondingPoints ;
-global x =  get(handles.edit1,'String');
-global y =  get(handles.edit2,'String');
-
-correspondingPoints=strsplit(y,',');
- 
-points = strsplit(x,',');
-
-order= get(handles.edit3,'String');
-chosenMethod= get(handles.text6,'String');
-%NASSER S
-switch chosenMethod
-    case "1- Newton"
-        % execute newton here NASSER
-    case "2- Larange"
-          % execute larange here NASSER
-end
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
 
 
 
@@ -213,28 +142,64 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
 end
 
 
-% --- Executes on button press in pushbutton3.
-function pushbutton3_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton3 (see GCBO)
-[baseName, folder] = uigetfile();
-try
-fullFileName = fullfile(folder, baseName);
-A  = Read(fullFileName);
-global method;
-global Eqs;
-global iter;
-global eps;
-global numbers;
-global corresponding;
+% --- Executes on selection change in listbox1.
+function listbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+index = get(hObject,'Value');
+list = get(hObject,'String');
+method = list{index}; 
+set(handles.text2,'String',method);
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
 
-method = A{1};
-Eqs = A{2};
-iter = A{3};
-eps = A{4};
-numbers = A{5};
-corresponding = A{6};
-catch
-err_message = msgbox('File is not chosen');
+% Hints: contents = cellstr(get(hObject,'String')) returns listbox1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from listbox1
+
+
+% --- Executes during object creation, after setting all properties.
+function listbox1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to listbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+global chosenMethod;
+global order;
+global points;
+global correspondingPoints ;
+x =  get(handles.edit1,'String');
+y =  get(handles.edit2,'String');
+chosenMethod = "1- Newton";
+correspondingPoints=strsplit(y,',');
+ 
+points = strsplit(x,',');
+order= get(handles.edit3,'String');
+
+chosenMethod= get(handles.text2,'String');
+disp(chosenMethod)
+
+%NASSER SWITCH DEPENED ON METHOD CHOSEN
+switch chosenMethod
+    case "1- Newton"
+        % execute newton here NASSER
+    case "2- Larange"
+          % execute larange here NASSER
+end
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
